8f8270d8420f5d9233755ede1d5b29987caa8035
William Leung <leungwk25@gmail.com>
2018-02-11 22:32:38 -0800

    completed checkWinner function

--- script.js
+++ script.js
@@ -1,83 +1,114 @@
-//to win [r1c1,r1c2,r1c3],[r2c1,r2c2,r2c3],[r3c1,r3c2,r3c3]
-//       [r1c1,r2c1,r3c1],[r1c2,r2c2,r3c2],[r1c3,r2c3,r3c3]
-//       [r1c1,r2c2,r3c3],[r3c1,r2c2,r1c3],
-let grumpy = 'https://a.wattpad.com/useravatar/GrumpyKittyCat1212.128.403917.jpg';
-let doge = 'https://78.media.tumblr.com/avatar_8882900ce8af_128.pnj';
-let player = grumpy;
-counter = 9;
-let gridboard = [null,null,null,null,null,null,null,null,null];
-
+const grumpy = 'https://a.wattpad.com/useravatar/GrumpyKittyCat1212.128.403917.jpg';
+const doge = 'https://78.media.tumblr.com/avatar_8882900ce8af_128.pnj';
+let humanAvatar = grumpy;
+//total possible wins
+const winSets = [
+  [
+    0, 1, 2
+  ],
+  [
+    3, 4, 5
+  ],
+  [
+    6, 7, 8
+  ],
+  [
+    0, 3, 6
+  ],
+  [
+    1, 4, 7
+  ],
+  [
+    2, 5, 8
+  ],
+  [
+    0, 4, 8
+  ],
+  [
+    2, 4, 6
+  ]
+];
+const sizeToWin = 3;
+let human = new Array();
+let computer = new Array();
+let humanTurn = true; //human's turn
 let positions = document.getElementsByTagName('td');
 
-for (let i = 0; i < positions.length; i++){
-  positions[i].addEventListener('click', positionClicked);
+function setGame() {
+  for (let i = 0; i < positions.length; i++) {
+    positions[i].addEventListener('click', getHumanMove);
+  }
 }
 
-function positionClicked(){
-  counter--;
-  this.innerHTML= '<img src='+player +'>';
-  this.removeEventListener('click', positionClicked)
-  let index = this.getAttribute('data-key');
-  gridboard[index] = true;
-  checkGrid();
-}
-//[012,345,678,036,147,258,048,246]
-function checkGrid(){ //check three in a row
-  if(counter ==0){ //draw}
+function getHumanMove() {
+  this.innerHTML = '<img src=' + humanAvatar + '>';
+  this.removeEventListener('click', getHumanMove)
+  let index = parseInt(this.getAttribute('data-key'));
+  human.push(index);
+  human.sort((a, b) => a - b);
 
-    if(gridboard[0] == true && gridboard[1] == true && gridboard[3] == true){
-      console.log('p 012 - vertical')
-    }
-    else if (gridboard[3] == true && gridboard[4] == true && gridboard[5] == true){
-      console.log('p 345 - vertical')
-    }
-    else if (gridboard[6] == true && gridboard[7] == true && gridboard[8] == true){
-      console.log('p 678 - vertical')
-    }
-    else if (gridboard[0] == true && gridboard[3] == true && gridboard[6] == true){
-      console.log('p 036 - horizontal')
-    }
-    else if (gridboard[1] == true && gridboard[4] == true && gridboard[7] == true){
-      console.log('p 147 - horizontal')
-    }
-    else if (gridboard[2] == true && gridboard[5] == true && gridboard[8] == true){
-      console.log('p 258 - horizontal')
-    }
-    else if (gridboard[0] == true && gridboard[4] == true && gridboard[8] == true){
-      console.log('p 048 - diagonal')
-    }
-    else if (gridboard[2] == true && gridboard[4] == true && gridboard[6] == true){
-      console.log('p 246 - diagonal')
-    }
-
-    else if(gridboard[0] == false && gridboard[1] == false && gridboard[3] == false){
-      console.log('012 - vertical')
-    }
-    else if (gridboard[3] == false && gridboard[4] == false && gridboard[5] == false){
-      console.log('345 - vertical')
-    }
-    else if (gridboard[6] == false && gridboard[7] == false && gridboard[8] == false){
-      console.log('678 - vertical')
-    }
-    else if (gridboard[0] == false && gridboard[3] == false && gridboard[6] == false){
-      console.log('036 - horizontal')
-    }
-    else if (gridboard[1] == false && gridboard[4] == false && gridboard[7] == false){
-      console.log('147 - horizontal')
-    }
-    else if (gridboard[2] == false && gridboard[5] == false && gridboard[8] == false){
-      console.log('258 - horizontal')
-    }
-    else if (gridboard[0] == false && gridboard[4] == false && gridboard[8] == false){
-      console.log('048 - diagonal')
-    }
-    else if (gridboard[2] == false && gridboard[4] == false && gridboard[6] == false){
-      console.log('246 - diagonal')
-    }
+  if (isWinner()) {
+    console.log('human win');
+  } else {
+    humanTurn = false;
+    getComputerMove();
+  }
 }
 
-function botPick(){
-  counter--;
-  
-
+function getComputerMove() {
+  let random = Math.floor()
+  this.innerHTML = '<img src=' + computerAvatar + '>';
+  this.removeEventListener('click', getHumanMove)
+  let index = parseInt(this.getAttribute('data-key'));
+  computer.push(index);
+  computer.sort((a, b) => a - b);
+  if (isWinner()) {
+    console.log('computer win');
+  } else {
+    humanTurn = true;
+  }
 }
+
+function isWinner() { //check three in a row
+  let win = false;
+  let checkArray;
+  humanTurn == true
+    ? checkArray = human
+    : checkArray = computer;
+  if (checkArray.length >= sizeToWin) {
+    for (let i = 0; i < winSets.length; i++) {
+      let set = winSets[i]; //winning hand e.g.[0,1,2]
+      let setFound = true; // this determines if we find the winning set
+
+      for (let j = 0; j < set.length; j++) {
+        // check if number is in current players hand
+        // if not, break, not winner
+        let found = false;
+        for (let k = 0; k < checkArray.length; k++) {
+          if (set[j] === checkArray[k]) { // e.g check if 0..1..2 is found in checkArray at positon 0
+            found = true;
+            break; //break k for loop and then search next set[1]
+          }
+        }
+        // value not found in players hand
+        // not a valid set, move on to next set to check
+        if (found == false) {
+          setFound = false;
+          break; // break j for loop
+        }
+      }
+      if (setFound == true) {
+        win = true;
+        break;
+      }
+    }
+  }
+  return win;
+}
+
+function reset() {
+  human = [];
+  computer = [];
+  setGame();
+}
+window.onload = setGame;
